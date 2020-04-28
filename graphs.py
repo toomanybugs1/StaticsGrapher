7fae67c212294d0d852bc8c063a4e4d8b072066f
Toomanybugs1 <treyshaw81@gmail.com>
2020-04-28 16:19:15 -0500

    padding ends

--- graphs.py
+++ graphs.py
@@ -8,7 +8,7 @@ class ShearDiagram():
         self.__loads = [] #list of tuples (int, float)
         self.__supports = [] #list of tuples (int, float)
 
-        for i in range(0, beam_length):
+        for i in range(0, beam_length + 1):
             self.__loads.append((-1, -1))
             self.__supports.append((-1, -1))
 
@@ -89,11 +89,15 @@ class ShearDiagram():
 
         self.__supports[support_pos] = (support_type, support_force)
 
+    # SHEAR GRAPH PLOTTER
     def plot_shear(self):
         cur_x = 0
         cur_y = 0
         x_vals = []
         y_vals = []
+
+        x_vals.append(-2)
+        y_vals.append(0)
 
         for i in range(0, self.__beam_length):
             support = self.__supports[i]
@@ -109,11 +113,16 @@ class ShearDiagram():
             x_vals.append(i)
             y_vals.append(cur_y)
 
+
+        x_vals.append(self.__beam_length)
+        y_vals.append(cur_y)
+        x_vals.append(self.__beam_length)
+        y_vals.append(0)
+        x_vals.append(self.__beam_length + 2)
+        y_vals.append(0)
+
         plt.plot(x_vals, y_vals)
         plt.show()
-
-    def plot_moment(self):
-        print("not written")
 
     def process_shear_load(self, load, cur_y):
         if (load[0] > -1):
@@ -122,71 +131,11 @@ class ShearDiagram():
         return cur_y
 
     def process_shear_support(self, support, cur_y):
-        if (support[0] > -1 && support[0] != 4):
+        if (support[0] > -1 and support[0] != 4):
             cur_y += support[1]
 
         return cur_y
 
-class MomentDiagram():
-
-    def __init__(self, beam_length):
-        self.__beam_length = beam_length;
-        self.__loads = list() #list of tuples (int, float, float)
-        self.__supports = list() #list of tuples (int, float)
-        self.__moments = list() #list of floats
-
-    def __str__(self):
-        retStr = "\nBeam Length: " + str(self.__beam_length) + " meters\n\n"
-        retStr += "Loads:\n"
-        for load in self.__loads:
-            retStr += "(Type: " + str(load[0]) + ", X: " + str(load[1]) + ", Magnitude: "+ str(load[2]) + "kN)\n"
-
-        retStr += "\nSupports:\n"
-        for sup in self.__supports:
-            retStr += "(Type: " + str(sup[0]) + ", X: " + str(sup[1]) + ")\n"
-
-        retStr += "\nMoments:\n"
-        for moment in self.__moments:
-            retStr += str(moment) + " kN/m\n"
-
-        return retStr
-
-    def add_load(self):
-        print("Load Types:")
-        print("1. Triangular")
-        print("2. Rectangular")
-        print("3. Point")
-        print("4. Free Moment\n")
-
-        load_type = 0
-        while (load_type > 4 or load_type < 1):
-            load_type = int(input("What type of load is this? "))
-
-        load_pos = -1
-        while (load_pos > self.__beam_length or load_pos < 0):
-            load_pos = float(input("What is the X position of the load? "))
-
-        load_force = float(input("What is the force of the load (kN)? "))
-
-        self.__loads.append((load_type, load_pos, load_force))
-
-    def add_support(self):
-        print("Support Types:")
-        print("1. Pin")
-        print("2. Roller")
-        print("3. Fixed\n")
-
-        support_type = 0
-
-        while (support_type > 3 or support_type < 1):
-            support_type = int(input("What type of support is this? "))
-
-        support_pos = -1
-        while (support_pos > self.__beam_length or support_pos < 0):
-            support_pos = float(input("What is the X position of the support? "))
-
-        self.__supports.append((support_type, support_pos))
-
-    def add_moment(self):
-        moment = float(input("What is the moment you'd like to add (kN/m)? "))
-        self.__moments.append(moment)
+    # MOMENT GRAPH PLOTTER
+    def plot_moment(self):
+        print("not written")
--- main.py
+++ main.py
@@ -5,24 +5,20 @@ def main():
 
         print("\nMoment Diagram Grapher")
         print("----------------------")
-        print("1. Shear Force Diagram")
-        print("2. Bending Moment Diagram")
-        print("3. Exit\n")
+        print("1. Generate Diagram")
+        print("2. Exit\n")
 
         menu_selection = input("Which would you like to do? ")
 
         if (menu_selection == "1"):
             shear()
 
-        elif(menu_selection == "2"):
-            bending()
-
         else:
             break
 
 
 def shear():
-    beam_length = int(input("What is the beam length (whole meters)? "))
+    beam_length = int(input("\nWhat is the beam length (whole meters)? "))
     graph = Grapher.ShearDiagram(beam_length)
 
     while(1):
@@ -43,34 +39,6 @@ def shear():
             break
 
     graph.plot_shear()
-
-
-def bending():
-    beam_length = int(input("What is the beam length (whole meters)? "))
-    graph = Grapher.MomentDiagram(beam_length)
-
-    while(1):
-        print("Enter Graph Data:")
-        print("1. Add Load")
-        print("2. Add Support")
-        print("3. Add Moment")
-        print("4. Draw Graph\n")
-
-        selection = input("Which would you like to do? ")
-
-        if (selection == "1"):
-            graph.add_load()
-
-        elif (selection == "2"):
-            graph.add_support()
-
-        elif (selection == "3"):
-            graph.add_moment()
-
-        else:
-            break
-
-    print(str(graph))
-
+    graph.plot_moment()
 
 main()
